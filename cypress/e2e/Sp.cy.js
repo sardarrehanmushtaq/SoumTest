70612a1dbd511ea2f2950a39a4260438ba73f4f3
Sardar <rehan.mushtaq@markinson.com.au>
2023-05-15 08:42:05 +0500

    savealll

--- cypress/e2e/Sp.cy.js
+++ cypress/e2e/Sp.cy.js
@@ -4,6 +4,8 @@ import Customer from "../pages/Customer.js"
 
 Cypress.on('uncaught:exception', () => false)
 
+describe('Test Suite for Sale Processing', () => {
+
 
 it('Sale Processing Test', function(){
     const lp=new Login();
@@ -64,8 +66,6 @@ it('Sale Processing Kitting Product', function(){
     sp.Okbutton()
     cy.wait(3000)
     sp.sellaskit()
-
-    
     sp.Apply()
    //sp.productprice()
    // sp.priceoverridereason()
@@ -82,27 +82,4 @@ it('Sale Processing Kitting Product', function(){
 
 
 })
-
-
-it('Customer Test', function(){
-
-    const lp=new Login();
-    const sp=new Sale();
-    const cp=new Customer();
-    const customernumber=Math.floor(Math.random() * 10000);
-    const customername=Math.floor(Math.random() * 10000);
-    lp.enterURL()
-    lp.enterUsername()
-    lp.enterPassword()
-    lp.clickLogin()
-    sp.clickburgermenu()
-    cp.clickaccountreceivaebles()
-    cp.clickcustomermaintenance()
-    cp.entercustomer()
-    cp.clickaddcustomer()
-    cp.entercustomernumber(customernumber)
-    cp.entercustomername(customername)
-    cp.clicktickbutton()
-
-
 })
