06cab513705d63d21cd524823afe1b9a54681363
sardarrehanmushtaq <sardarrehganmushtaq@gmail.com>
2024-02-13 17:55:13 +0500

    soum test

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl:"https://www.saucedemo.com/",
-    baseUrl2:"https://api.qa.soum.sa/api-v2/rest/api/v1/product/",
+  
     chromeWebSecurity: false,
   
 
